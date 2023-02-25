dea4d3adc8da0cc72a2cce3d6f146952103dd3df
x3rim <lordpunk4@gmail.com>
2023-02-25 13:35:14 +0300

    Home Work 1.4

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            Console.Write("Hello, ");
+            Console.Write("World");
+            Console.Write(" !!!");
+
+            Console.ReadLine();
+        }
+    }
+}
